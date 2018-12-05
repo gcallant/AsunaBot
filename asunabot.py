b7ea375ce066e92d6ea6e399c8bc0d8925f98ba1
gcallant <grant@grantcallant.com>
2018-12-04 18:15:09 -0800

    Fixed formatting issue (sigh, Python)

--- asunabot.py
+++ asunabot.py
@@ -1,16 +1,14 @@
 # Work with Python 3.6
 import asyncio
-import datetime
-
 import discord
+import datetime
 from discord.ext.commands import Bot
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 from sqlalchemy.sql.expression import true
-
+from sqlalchemy.orm import sessionmaker
 from asunabot_declative import Event, PlayerSignup, Roster, Base
 
-engine = create_engine('sqlite:///asunabot.db')
+engine = create_engine('sqlite:///synthbot.db')
 Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
@@ -18,17 +16,16 @@ session = DBSession()
 # CONSTANTS
 TOKEN = 'NTE4NTUzNTcyNDI2NjQ1NTA0.DuShbw.TwNTD0i5vvgjbM27QtHCYG3vY44'
 SERVER_ID = '269224197299896320'
-# '269224197299896320'
+#'269224197299896320'
 SIGNUP_LOG_CHANNEL_ID = '498328294366642195'
-# '498328294366642195'
+#'498328294366642195'
 BOT_PREFIX = ("?")
 PLAYER_ROLES = {
    'tank',
    'heal',
-   'heals',
    'mdps',
    'rdps',
-   #Removed Flex'flex',
+   'flex',
    'reserve'
    }
 
@@ -49,10 +46,10 @@ PLAYER_ROLES_DATA = {
       'emoji': ':bow_and_arrow:',
       'display_name': 'Ranged'
       },
-   # 'flex': {
-   #    'emoji': ':question:',
-   #    'display_name': 'Flex'
-   #    },
+   'flex': {
+      'emoji': ':question:',
+      'display_name': 'Flex'
+      },
    'reserve': {
       'emoji': ':fingers_crossed:',
       'display_name': 'Reserve'
@@ -97,22 +94,22 @@ async def player_signup(context, player_role, *flex_roles_args):
             'Sorry, I am no longer supporting the flex and reserve role.'
             )
          return
-      # flex_roles = None
-      # if cleaned_player_role == 'flex' or cleaned_player_role == 'reserve':
-      #    flex_roles = ' '.join(flex_roles_args).strip().lower()
+      flex_roles = None
+      if cleaned_player_role == 'flex' or cleaned_player_role == 'reserve':
+         flex_roles = ' '.join(flex_roles_args).strip().lower()
 
       if cleaned_player_role in PLAYER_ROLES:
          existing_player_signup = session.query(PlayerSignup).get((context.message.author.id, event.channel_id))
          if existing_player_signup:
             existing_player_signup.player_roles = cleaned_player_role
-            # existing_player_signup.flex_roles = flex_roles
+            existing_player_signup.flex_roles = flex_roles
          else:
             new_player_signup = PlayerSignup(
                id=context.message.author.id,
                player_name=context.message.author.display_name,
                player_mention=context.message.author.mention,
                player_roles=cleaned_player_role,
-               # flex_roles=flex_roles,
+               flex_roles=flex_roles,
                event=event,
                )
             session.add(new_player_signup)
@@ -150,16 +147,8 @@ async def create_event(context):
          return msg.content.strip()
 
       event_name = await ask_user("What do you want to name the event?", author, client)
-      valid = False
-      while not valid:
-         try:
-
-            event_day_raw = await ask_user("What day is the event (ex. 06/24/1990)?", author, client)
-            event_day = datetime.datetime.strptime(event_day_raw, "%m/%d/%Y")
-            valid = True
-         except ValueError as e:
-            print("Sorry, the date you entered was not in a valid format, please try again.")
-            #TODO: Add some logging here
+      event_day_raw = await ask_user("What day is the event (ex. 06/24/1990)?", author, client)
+      event_day = datetime.datetime.strptime(event_day_raw, "%m/%d/%Y")
       event_time = await ask_user("What time is the event (in CST)?", author, client)
       event_leader = await ask_user("Who is leading the event?", author, client)
       num_of_tanks = await ask_user("How many TANKS for the event?", author, client)
@@ -178,8 +167,7 @@ async def create_event(context):
       session.add(new_roster)
       session.commit()
 
-      new_channel = await client.create_channel(client.get_server(SERVER_ID), event_name,
-                                                type=discord.ChannelType.text)
+      new_channel = await client.create_channel(client.get_server(SERVER_ID), event_name, type=discord.ChannelType.text)
       await client.send_message(author, 'Created new channel for event with name: ' + event_name)
 
       new_event = Event(
@@ -197,156 +185,134 @@ async def create_event(context):
       session.add(new_event)
       session.commit()
 
-      channel_message = f'@everyone\nDate: {event_day_raw}\nTime: {event_time} Central\n{event_description}\n\nRaid ' \
-         f'Leader:{event_leader} \n\n\n{get_event_details(
-      new_channel.id)}'
+      channel_message = f'@everyone\nDate: {event_day_raw}\nTime: {event_time} Central\n{event_description}\n\nRaid Leader:{event_leader} \n\n\n{get_event_details(new_channel.id)}'
       channel_info_message = await client.send_message(new_channel, channel_message)
 
       event = session.query(Event).get(new_channel.id)
       event.channel_info_message = channel_info_message.id
       session.commit()
 
+async def update_channel_info_message(event_id):
+   event = session.query(Event).get(event_id)
+   channel = client.get_channel(event_id)
+   channel_message = await client.get_message(channel, event.channel_info_message)
+   new_message_content = f'@everyone\nDate: {event.event_day.split()[0]}\nTime: {event.event_time} Central\n{event.event_description}\n\nRaid Leader:{event.event_leader} \n\n\n{get_event_details(event_id)}'
+   channel_info_message = await client.edit_message(channel_message, new_content=new_message_content)
+   event.channel_info_message = channel_info_message.id
+   session.commit()
 
-      async
 
-      def update_channel_info_message(event_id):
-         event = session.query(Event).get(event_id)
-         channel = client.get_channel(event_id)
-         channel_message = await
-         client.get_message(channel, event.channel_info_message)
-         new_message_content = f'@everyone\nDate: {event.event_day.split()[
-         0]}\nTime: {event.event_time}
-         Central\n
-         {event.event_description}\n\nRaid
-         Leader: {event.event_leader} \n\n\n
-         {get_event_details(
-            event_id)}
-         '
-         channel_info_message = await
-         client.edit_message(channel_message, new_content=new_message_content)
-         event.channel_info_message = channel_info_message.id
-         session.commit()
+def get_highest_discord_role(player_id):
+   discord_role = client.get_server(SERVER_ID).get_member(player_id).top_role
+   return discord_role.name
 
-   def get_highest_discord_role(player_id):
-      discord_role = client.get_server(SERVER_ID).get_member(player_id).top_role
-      return discord_role.name
 
-   @client.command(pass_context=True)
-   async def tell_them(context):
-      await client.say(
-         "My master is currently testing me, please leave me alone. I'm looking at you Doc and Captain Hammer.")
+@client.command(pass_context=True)
+async def tell_them(context):
+   await client.say("My master is currently testing me, please leave me alone. I'm looking at you Doc and Captain Hammer.")
 
-   @client.command(pass_context=True)
-   async def tell_them_more(context):
-      await client.say("My master he...he...he...I CAN'T SAY")
+@client.command(pass_context=True)
+async def tell_them_more(context):
+   await client.say("My master he...he...he...I CAN'T SAY")
 
-   @client.command(name='event-details',
-                   description='Check details for this event.',
-                   brief='Check details for this event.',
-                   pass_context=True)
-   async def event_details(context, extra=None):
-      event_details = get_event_details(context.message.channel.id, extra)
-      await client.say(event_details)
+@client.command(name='event-details',
+                description='Check details for this event.',
+                brief='Check details for this event.',
+                pass_context=True)
+async def event_details(context, extra=None):
+   event_details = get_event_details(context.message.channel.id, extra)
+   await client.say(event_details)
 
-   def get_event_details(event_id, extra=None):
-      event = session.query(Event).get(event_id)
-      if event:
-         signups = session.query(PlayerSignup).filter(PlayerSignup.event_id == event.channel_id).order_by(
-            PlayerSignup.date_created.asc()).all()
-         if signups and len(signups) > 0:
-            signups_by_role = {}
-            for signup in signups:
-               signups_by_role[signup.player_roles] = signups_by_role.get(signup.player_roles, []) + [signup]
 
-            result = ""
-            for player_role, player_role_data in PLAYER_ROLES_DATA.items():
-               player_names_joined = None
-               if player_role in signups_by_role:
-                  signups = signups_by_role[player_role]
-                  player_names_joined = "\n".join([
-                     f'\t {signup.player_mention} {"(Roles: " + signup.flex_roles + ")" if signup.flex_roles else ""} ('
-                     f'Rank:{get_highest_discord_role(
-                  signup.id)}) {signup.date_created.strftime("- %Y-%m-%d %H:%M") if extra == "extra" else ""}
-                  '
-               for signup in signups])
-               if player_names_joined:
-                  result += f'{player_role_data["emoji"]} {player_role_data[
-               "display_name"]}: \n
-               {player_names_joined}\n\n
-               '
-               else:
-               result += f'{player_role_data["emoji"]} {player_role_data[
-               "display_name"]}: \n\tNo
-               players
-               signed
-               up
-               for this role\n\n'
+def get_event_details(event_id, extra=None):
+   event = session.query(Event).get(event_id)
+   if event:
+      signups = session.query(PlayerSignup).filter(PlayerSignup.event_id == event.channel_id).order_by(
+         PlayerSignup.date_created.asc()).all()
+      if signups and len(signups) > 0:
+         signups_by_role = {}
+         for signup in signups:
+            signups_by_role[signup.player_roles] = signups_by_role.get(signup.player_roles, []) + [signup]
 
-      return f'The current players signed up are: \n{result}'
+         result = ""
+         for player_role, player_role_data in PLAYER_ROLES_DATA.items():
+            player_names_joined = None
+            if player_role in signups_by_role:
+               signups = signups_by_role[player_role]
+               player_names_joined = "\n".join([
+                  f'\t {signup.player_mention} {"(Roles: " + signup.flex_roles + ")" if signup.flex_roles else ""} (Rank:{get_highest_discord_role(signup.id)}) {signup.date_created.strftime("- %Y-%m-%d %H:%M") if extra=="extra" else ""}'
+                  for signup in signups])
+            if player_names_joined:
+               result += f'{player_role_data["emoji"]} {player_role_data["display_name"]}: \n{player_names_joined}\n\n'
+            else:
+               result += f'{player_role_data["emoji"]} {player_role_data["display_name"]}: \n\tNo players signed up for this role\n\n'
 
-   else:
-   return 'No players have signed up for this event.'
-   else:
-   return 'No event has been created for this channel.'
-
-   @client.command(name='myevents',
-                   description='Shows events you signed up for.',
-                   brief='Shows events you signed up for.',
-                   pass_context=True)
-   async def my_events(context):
-      signups = session.query(PlayerSignup).filter(PlayerSignup.id == context.message.author.id).filter(
-         PlayerSignup.event.has(active=true())).all()
-
-      if signups is None or len(signups) == 0:
-         await client.send_message(context.message.author,
-                                   f'{context.message.author.mention}, you are not signed up for any events.')
+         return f'The current players signed up are: \n{result}'
       else:
-         result = "\n".join(
-            [f'* {client.get_channel(signup.event.channel_id).name} - {signup.player_roles}' for signup in signups])
-         await client.send_message(context.message.author,
-                                   f'{context.message.author.mention}, your current events are: \n{result}')
+         return 'No players have signed up for this event.'
+   else:
+      return 'No event has been created for this channel.'
 
-   @client.command(name='cancel',
-                   description='Cancels your signup for the current event.',
-                   brief='Cancel signup',
-                   pass_context=True)
-   async def cancel_signup(context):
-      event_id = context.message.channel.id
-      player_id = context.message.author.id
-      event = session.query(Event).get(event_id)
-      existing_player_signup = session.query(PlayerSignup).get((player_id, event_id))
-      if existing_player_signup:
-         session.delete(existing_player_signup)
-         session.commit()
-         await client.say(f'{context.message.author.mention}, you are no longer signed up for this event.')
-         await update_channel_info_message(event_id)
-      else:
-         await client.say(f'{context.message.author.mention}, my records show you never signed up for this event.')
+@client.command(name='myevents',
+                description='Shows events you signed up for.',
+                brief='Shows events you signed up for.',
+                pass_context=True)
+async def my_events(context):
+   signups = session.query(PlayerSignup).filter(PlayerSignup.id == context.message.author.id).filter(
+      PlayerSignup.event.has(active=true())).all()
 
-   @client.event
-   async def on_channel_delete(channel):
-      existing_event = session.query(Event).get(channel.id)
-      if existing_event:
-         existing_event.active = False
-         session.commit()
-         await client.send_message(client.get_channel(SIGNUP_LOG_CHANNEL_ID),
-                                   f'The following event was marked inactive due to channel removal: {channel.name}.')
+   if signups is None or len(signups) == 0:
+      await client.send_message(context.message.author, f'{context.message.author.mention}, you are not signed up for any events.')
+   else:
+      result = "\n".join([f'* {client.get_channel(signup.event.channel_id).name} - {signup.player_roles}' for signup in signups])
+      await client.send_message(context.message.author, f'{context.message.author.mention}, your current events are: \n{result}')
 
-   @client.event
-   async def on_ready():
-      print('Logged in as')
-      print(client.user.name)
-      print(client.user.id)
+
+@client.command(name='cancel',
+                description='Cancels your signup for the current event.',
+                brief='Cancel signup',
+                pass_context=True)
+async def cancel_signup(context):
+   event_id = context.message.channel.id
+   player_id = context.message.author.id
+   event = session.query(Event).get(event_id)
+   existing_player_signup = session.query(PlayerSignup).get((player_id, event_id))
+   if existing_player_signup:
+      session.delete(existing_player_signup)
+      session.commit()
+      await client.say(f'{context.message.author.mention}, you are no longer signed up for this event.')
+      await update_channel_info_message(event_id)
+   else:
+      await client.say(f'{context.message.author.mention}, my records show you never signed up for this event.')
+
+
+@client.event
+async def on_channel_delete(channel):
+   existing_event = session.query(Event).get(channel.id)
+   if existing_event:
+      existing_event.active = False
+      session.commit()
+      await client.send_message(client.get_channel(SIGNUP_LOG_CHANNEL_ID),
+                                f'The following event was marked inactive due to channel removal: {channel.name}.')
+
+
+@client.event
+async def on_ready():
+   print('Logged in as')
+   print(client.user.name)
+   print(client.user.id)
+   print('------')
+
+
+async def list_servers():
+   await client.wait_until_ready()
+   while not client.is_closed:
+      print("Current servers:")
+      for server in client.servers:
+         print(server.name)
       print('------')
+      await asyncio.sleep(600)
 
-   async def list_servers():
-      await client.wait_until_ready()
-      while not client.is_closed:
-         print("Current servers:")
-         for server in client.servers:
-            print(server.name)
-         print('------')
-         await asyncio.sleep(600)
 
-   client.loop.create_task(list_servers())
-   client.run(TOKEN)
+client.loop.create_task(list_servers())
+client.run(TOKEN)
