5903b4eb1b599c96beacaab568c9075a6d9e3cb4
gcallant <grant@grantcallant.com>
2019-01-05 23:15:12 -0800

    Added ability to cancel event creation

--- asunabot.py
+++ asunabot.py
@@ -6,11 +6,12 @@ import asyncio
 import platform
 import discord
 import datetime
+from datetime import timedelta
 from discord.ext.commands import Bot
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 from sqlalchemy.sql.expression import true
 from sqlalchemy.orm import sessionmaker
-from asunabot_declative import Event, PlayerSignup, Roster, Base
+from asunabot_declative import Event, PlayerSignup, Reminder, Roster, Base
 
 engine = create_engine('sqlite:///asunabot.db')
 Base.metadata.bind = engine
@@ -112,7 +113,7 @@ async def player_signup(context, player_role, *flex_roles_args):
       cleaned_player_role = player_role.strip().lower()
       if cleaned_player_role == 'flex' or cleaned_player_role == 'reserve':
          await client.say(
-            'Sorry, I am no longer supporting the flex and reserve role.'
+            'ごめんなさい, I am no longer supporting the flex and reserve role.'
             )
          return
       #Allows users to also type heals without adding an additional dictionary entry
@@ -144,10 +145,10 @@ async def player_signup(context, player_role, *flex_roles_args):
          await update_channel_info_message(event_id, context)
       else:
          await client.say(
-            'Sorry, I do not recognize that role. Please try one of the following roles: ' + ', '.join(
+            'ごめんなさい, I do not recognize that role. Please try one of the following roles: ' + ', '.join(
                PLAYER_ROLES))
    else:
-      await client.say("Oops, it looks like there wasn't an event created for this channel.")
+      await client.say("せみません, it looks like there wasn't an event created for this channel.")
 
 
 @client.command(name='edit',
@@ -155,6 +156,7 @@ async def player_signup(context, player_role, *flex_roles_args):
                 brief='Edits this event.',
                 pass_context=True)
 async def edit_event(context):
+   client.say("ごめんなさい, functionality has not yet been implemented for this feature.")
    author = await check_permissions(context)
    if not author:
       return
@@ -171,16 +173,17 @@ async def edit_event(context):
    if event:
       await update_channel_info_message(event_id, context)
    else:
-      await client.say("Oops, it looks like there wasn't an event created for this channel.")
+      await client.say("せみません, it looks like there wasn't an event created for this channel.")
 
 
 async def check_permissions(context):
    if not context.message.author.server_permissions.administrator:
       await client.send_message(context.message.author,
-                                "Sorry, I don't seem to have you on my list of admins. If this is an error "
+                                "ごめんなさい, I don't seem to have you on my list of admins. If this is an error "
                                 "please contact a Thane and let them know you need admin permissions.")
    else:
       return context.message.author
+
 
 
 @client.command(name='event',
@@ -200,34 +203,41 @@ async def create_event(context):
    async def ask_user(question, author, client):
       await client.send_message(author, question)
       msg = await client.wait_for_message(author=author, check=check)
-      return msg.content.strip()
-
-   event_name = await ask_user("What do you want to name the event?", author, client)
+      data = msg.content.strip()
+      if data == '?cec':
+         raise InterruptedError
+      return data
 
    async def askUserChecked(message, author, client, function, format, exceptionMessage):
       valid = False
       while not valid:
-        try:
+         try:
             raw_data = await ask_user(message, author, client)
             data = function(raw_data, format)
             return data
-        except:
+         except ValueError:
             await client.send_message(author, exceptionMessage)
+   try:
+      event_name = await ask_user("What do you want to name the event?", author, client)
 
-   event_day = await askUserChecked(message="What day is the event (ex. 06/24/1990)?", author=author, client=client,
-                              function=datetime.datetime.strptime, format="%m/%d/%Y",
-                              exceptionMessage="Sorry, you entered the date in an unrecognized format, try again with MM/DD/YYYY\n")
+      event_day = await askUserChecked(message="What day is the event (ex. 06/24/1990)?", author=author, client=client,
+                                 function=datetime.datetime.strptime, format="%m/%d/%Y",
+                                 exceptionMessage="ごめんなさい, you entered the date in an unrecognized format, try again with MM/DD/YYYY\n")
 
-   event_time = await askUserChecked("What CST time is the event in 24 hour format (ex. 1800 for 6:00PM)?",
-                                     author, client, datetime.datetime.strptime, "%H",
-                                     "Sorry, you entered the time in an unrecognized format, try again with HHHH (24 Hour)\n")
-   event_leader = await ask_user("Who is leading the event?", author, client)
-   num_of_tanks = await ask_user("How many TANKS for the event?", author, client)
-   num_of_heals = await ask_user("How many HEALERS for the event?", author, client)
-   num_of_mdps = await ask_user("How many MELEE DPS for the event?", author, client)
-   num_of_rdps = await ask_user("How many RANGED DPS for the event?", author, client)
-   event_description = await ask_user("What is the event description?", author, client)
-   event_rank = await ask_user("What is the lowest rank that can apply for the event?", author, client)
+      event_time = await askUserChecked("What CST time is the event in 24 hour format (ex. 1800 for 6PM)?",
+                                        author, client, datetime.datetime.strptime, "%H%M",
+                                        "ごめんなさい, you entered the time in an unrecognized format, try again with HHHH (24 Hour)\n")
+      event_leader = await ask_user("Who is leading the event?", author, client)
+      num_of_tanks = await ask_user("How many TANKS for the event?", author, client)
+      num_of_heals = await ask_user("How many HEALERS for the event?", author, client)
+      num_of_mdps = await ask_user("How many MELEE DPS for the event?", author, client)
+      num_of_rdps = await ask_user("How many RANGED DPS for the event?", author, client)
+      event_description = await ask_user("What is the event description?", author, client)
+      event_rank = await ask_user("What is the lowest rank that can apply for the event?", author, client)
+   except InterruptedError:
+      await client.send_message(author, "Event creation has been canceled.")
+      print(f'Event creation was canceled by {author} {datetime.datetime.now()}')
+      return
 
    new_roster = Roster(
       max_tanks=num_of_tanks,
@@ -267,7 +277,7 @@ async def update_channel_info_message(event_id, context):
    event = session.query(Event).get(event_id)
    channel = client.get_channel(event_id)
    channel_message = await client.get_message(channel, event.channel_info_message)
-   new_message_content = f'@everyone\nDate: {event.event_day.split()[0]}\nTime: {event.event_time.split()[1]} Central\n{event.event_description}\n\nRaid Leader:{event.event_leader} \n\n\n{get_event_details(event_id, context)}'
+   new_message_content = f'@everyone\nDate: {str(event.event_day).split()[0]}\nTime: {str(event.event_time).split()[1]} Central\n{event.event_description}\n\nRaid Leader:{event.event_leader} \n\n\n{get_event_details(event_id, context)}'
    channel_info_message = await client.edit_message(channel_message, new_content=new_message_content)
    event.channel_info_message = channel_info_message.id
    session.commit()
@@ -278,7 +288,7 @@ def set_reminders():
    # eventNumReminders = await askUserChecked("How many reminders would you like to make (specify a number 0-3)\n"
    #                                          "You'll be asked when you'd like to set them in the follow question.",
    #                                          author, client, int, 10,
-   #                                          "Sorry, you entered the number in an unrecognized format, try again with 0-3\n")
+   #                                          "ごめんなさい, you entered the number in an unrecognized format, try again with 0-3\n")
    #
    # def numberOrdinal(number):
    #    if number == 1:
@@ -298,7 +308,7 @@ def set_reminders():
    #                                                    "signed up for the event at 4:00PMCST on 12/07/2018)".format(numberOrdinal(event + 1)),
    #                                                    author, client, datetime.datetime.strptime,
    #                                                    "%m/%d/%Y/%H/%M",
-   #                                                    "Sorry, you entered the date in an unrecognized format, try again with MM/DD/YYYY/HHHH/MM\n")
+   #                                                    "ごめんなさい, you entered the date in an unrecognized format, try again with MM/DD/YYYY/HHHH/MM\n")
 
 
 def get_highest_discord_role(player_id, context):
@@ -322,6 +332,16 @@ def get_highest_discord_role(player_id, context):
 async def event_details(context, extra=None):
    event_details = get_event_details(context.message.channel.id, context, extra)
    await client.say(event_details)
+
+
+@client.command(name='cec',
+                description='When used in a message to the bot, during making an event, it will stop the current '
+                            'event that is being created.',
+                brief='Cancels creating the current event.',
+                pass_context=True)
+async def cancelEventCreation(context):
+   if not context.message.channel.is_private:
+      await client.say("ごめんなさい, this command will only work during an event creation.")
 
 
 def get_event_details(event_id, context, extra=None):
@@ -403,15 +423,129 @@ async def on_ready():
    print(client.user.id)
    print('------')
 
+def defaultMessage(event, player, time):
+   return f"こんにちわ {player.player_name},\n\nThis is a friendly {time} reminder that you signed up for the event:\n\n\
+```{event.event_name} \non {str(event.event_day).split()[0]}\nat {str(event.event_time).split()[1]}CST\n\
+You signed up as a: {player.player_roles}```\n\n\
+For more information, please refer to the {event.event_name} channel.\nIf you cannot attend the event, or wish to cancel, please \
+use the command ?cancel in the {event.event_name} channel."
+
+def imminentMessage(event, player):
+   return f"こんにちわ, {player.player_name}, your event {event.event_name} starts in less than 15 minutes!\nPlease get logged in \
+on the character as a {player.player_roles} and x-up in chat in the next 5 minutes.\nPlease ensure your inventory is \
+clear and you have all appropriate gear, food, and potions needed for the run. We will be starting shortly!"
+
+def getMessageForTime(reminder, event, player):
+   if reminder == 0:
+      return defaultMessage(event, player, "48 hour")
+   elif reminder == 1:
+      return defaultMessage(event, player, "24 hour")
+   elif reminder == 2:
+      return defaultMessage(event, player, "2 hour")
+   elif reminder == 3:
+      return imminentMessage(event, player)
+   else:
+      print(f"Received an invalid reminder number {reminder} for event {event.event_name}")
+
+def markReminderSent(reminderNumber, eventID):
+   newReminder = session.query(Reminder).filter(Reminder.id == eventID).one_or_none()
+
+   if reminderNumber == 0:
+      if(newReminder == None):
+         newReminder = Reminder(id = eventID, first_reminder_sent=True, second_reminder_sent=False,
+                             third_reminder_sent=False, fourth_reminder_sent=False)
+      session.add(newReminder)
+      session.commit()
+   elif reminderNumber == 1:
+      if(newReminder == None):
+         newReminder = Reminder(id = eventID, first_reminder_sent=False, second_reminder_sent=True,
+                                third_reminder_sent=False, fourth_reminder_sent=False)
+         session.add(newReminder)
+      else:
+         newReminder.second_reminder_sent = True
+      session.commit()
+   elif reminderNumber == 2:
+      if(newReminder == None):
+         newReminder = Reminder(id = eventID, first_reminder_sent=False, second_reminder_sent=False,
+                                third_reminder_sent=True, fourth_reminder_sent=False)
+      else:
+         newReminder.third_reminder_sent = True
+      session.commit()
+   elif reminderNumber == 3:
+      if(newReminder == None):
+         newReminder = Reminder(id = eventID, first_reminder_sent=False, second_reminder_sent=False,
+                                third_reminder_sent=False, fourth_reminder_sent=True)
+      else:
+         newReminder.fourth_reminder_sent = True
+      session.commit()
+
+async def sendReminder(reminder, event):
+   players = session.query(PlayerSignup).filter(PlayerSignup.event_id == event.channel_id)
+
+   for player in players:
+      user = await client.get_user_info(player.id)
+      await client.send_message(destination=user, content=getMessageForTime(reminder, event, player))
+
+   markReminderSent(reminder, event.channel_id)
+
+def columFromNumber(reminder):
+   if reminder == 0:
+      return "first_reminder_sent"
+   elif reminder == 1:
+      return "second_reminder_sent"
+   elif reminder == 2:
+      return "third_reminder_sent"
+   elif reminder == 3:
+      return "fourth_reminder_sent"
+
+def reminderSent(reminderNumber, eventID):
+   reminder = session.query(Reminder).filter(Reminder.id == eventID).filter(text(columFromNumber(reminderNumber))).one_or_none()
+   return reminder
+
+def getTimeRange(event):
+  eventDay = event.event_day
+  eventTime = event.event_time
+  timeinTwoDays = datetime.datetime.now() + timedelta(hours=48)
+  timeinOneDay = datetime.datetime.now() + timedelta(hours=24)
+  timeinTwoHours = datetime.datetime.now() + timedelta(hours=2)
+  timeinFifteenMinutes = datetime.datetime.now() + timedelta(minutes=15)
+
+  if timeinTwoDays <= eventDay > timeinOneDay:
+     return 0
+  elif timeinOneDay <= eventDay > timeinTwoHours:
+     return 1
+  elif timeinTwoHours <= eventTime > timeinFifteenMinutes:
+     return 2
+  elif timeinFifteenMinutes <= eventTime:
+     return 3
+  else:
+     return 4
+
+
 async def checkReminders():
-   stub
+   events = getEventsToRemind()
+
+   for event in events:
+      timeRange = getTimeRange(event)
+      if timeRange < 0 or timeRange > 3:
+         return
+      eventID = event.channel_id
+      if not reminderSent(timeRange, eventID) or None:
+         await sendReminder(reminder, event)
+
+
+def getEventsToRemind():
+   timeInTwoDays = datetime.datetime.now() + timedelta(hours=48)
+   events = session.query(Event).filter(Event.active) \
+      .filter(Event.event_day <= timeInTwoDays)
+   return events
 
 
 async def runClient():
    await client.wait_until_ready()
    while not client.is_closed:
       print("Current servers:")
-      #checkReminders()
+      await checkReminders()
       for server in client.servers:
          print(server.name)
       print('------')
